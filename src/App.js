517fe3c4623c41885e21a379f874a266415ce391
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-07 16:20:46 +0700

    Add Class Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 // import {Greet} from './components/Greet' named import
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
