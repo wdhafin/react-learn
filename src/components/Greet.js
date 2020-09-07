8b33b9c2b508e994089553a3aa64e4e8f236f8fc
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-07 14:29:46 +0700

    Add functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+// without ES6
+// function Greet() {
+//     return <h1>Hello Your Name</h1>
+// }
+
+//with ES6
+
+//export const Greet = () => <h1>Hello Your Name</h1>
+const Greet = () => <h1>Hello Your Name</h1>
+
+export default Greet
