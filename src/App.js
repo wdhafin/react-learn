6b8b3376fed6b926c24aed5a5c6deba997180171
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-08 16:10:27 +0700

    Add Props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-// import Greet from './components/Greet';
-// import Welcome from './components/Welcome';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 import Hello from './components/Hello';
-// import {Greet} from './components/Greet' named import
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet/>
-      <Welcome/> */}
-      <Hello />
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wornder Wowan" />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wornder Wowan" />
+      {/* <Hello /> */}
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-// without ES6
-// function Greet() {
-//     return <h1>Hello Your Name</h1>
-// }
-
-//with ES6
-
-//export const Greet = () => <h1>Hello Your Name</h1>
-const Greet = () => <h1>Hello Your Name</h1>;
+const Greet = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        Hello {props.name} a.k.a {props.heroName}
+      </h1>
+      {props.children}
+    </div>
+  );
+};
 
 export default Greet;
