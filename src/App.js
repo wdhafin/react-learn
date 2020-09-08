4538f191873453f845fce5c39a51bd8f0ac64ce0
Muhammad Dhafin <work.dhafin@gmail.com>
2020-09-08 18:42:01 +0700

    Add sample state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wornder Wowan" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wornder Wowan" />
-      {/* <Hello /> */}
+      <Message />
     </div>
   );
 }
